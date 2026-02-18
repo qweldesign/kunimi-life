c47a7e9f413b74ae24ebbeedd32e34b76ac68c90
QWEL.DESIGN <webmaster@qwel.design>
2026-02-18 14:57:36 +0900

    Add Contact Form

--- init.js
+++ init.js
@@ -2,3 +2,7 @@
 import ActionCore from './js/action-core.js';
 new ActionCore.BackToTop();
 new ActionCore.SafeEmbed();
+
+// Contact Form
+import ContactForm from './js/contact-form.js';
+new ContactForm();
